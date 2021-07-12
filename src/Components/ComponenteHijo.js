2dc9af431507b44b250b3328b2c2dff838e47312
Joiss Ventura Rivera <2389790@gmail.com>
2021-07-12 17:52:33 -0500

    Se creo counter-app

---
+++ src/Components/ComponenteHijo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ComponenteHijo = () => {
+    return (
+        <div className = "mt-3">
+            <p>Soy un componente Hijo</p>
+        </div>
+    )
+}
---
+++ src/CounterApp.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { ComponenteHijo } from './Components/ComponenteHijo';
+
+import PropTypes from 'prop-types';
+
+import './CounterApp.css'
+
+export const CounterApp = ({valor}) => {
+    const [counterState, setcounterState] = useState(valor);
+
+    const aumentar = () => {
+        setcounterState( counterState + 1);
+    }
+
+    const reducir = () => {
+        setcounterState( counterState - 1);
+    }
+
+    const reset = () => {
+        setcounterState( valor );
+    }
+
+    return (
+        <div className = "d-flex-column">
+            <h2 className = "mb-3">Counter App ** {counterState}</h2>
+            <hr />
+            <button onClick = { () => {aumentar()}} 
+                className = "btn btn-outline-primary mr-3"> +1 </button>
+
+            <button onClick = { () => {reset()}} 
+                className = "btn btn-outline-primary mr-3"> Reset </button>
+
+            <button onClick = { () => {reducir()}} 
+                className = "btn btn-outline-primary"> -1 </button>
+                
+            <ComponenteHijo/>
+        </div>
+    )
+}
+CounterApp.propTypes = {
+    valor: PropTypes.number.isRequired
+}
